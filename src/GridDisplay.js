d1102b4f1fe5d5da3842f718f3848016c709fb1c
Evan Wright <emw8105@gmail.com>
2024-08-19 22:16:04 -0500

    profile picture gradient border

--- src/GridDisplay.js
+++ src/GridDisplay.js
@@ -10,32 +10,42 @@ const GridDisplay = ({ content, gridSize, includeProfilePicture, profilePictureU
   const contentToDisplay = content.slice(0, maxArtists);
 
   const backgroundStyle = useGradient
-    ? { background: `linear-gradient(${color1}, ${color2})`,
+    ? { background: `linear-gradient(to bottom right, ${color1}, ${color2})`,
         gridTemplateColumns: `repeat(${gridSize.x}, 1fr)`,
         gridTemplateRows: `repeat(${gridSize.y}, auto)`, }
     : {gridTemplateColumns: `repeat(${gridSize.x}, 1fr)`,
         gridTemplateRows: `repeat(${gridSize.y}, auto)`,};
-
-  return (
-    <div className="grid-container" style={backgroundStyle}>
-      {contentToDisplay.map((contentInstance, index) => {
-        const imageUrl = contentInstance.images
-          ? contentInstance.images[0].url
-          : contentInstance.album.images[0].url;
-
-        return (
-          <div key={index} className="grid-item">
-            <img src={imageUrl} alt="Grid content" />
-          </div>
-        );
-      })}
-      {includeProfilePicture && profilePictureUrl && (
-        <div className="profile-picture-overlay">
-          <img src={profilePictureUrl} alt="Profile" className="profile-picture" />
+        
+        const profilePictureContainerStyle = useGradient
+        ? {
+            backgroundImage: `linear-gradient(to bottom right, ${color1}, ${color2})`,
+          }
+        : {};
+      
+      const profilePictureStyle = {
+        borderRadius: '50%',
+      };
+      
+      return (
+        <div className="grid-container" style={backgroundStyle}>
+          {contentToDisplay.map((contentInstance, index) => {
+            const imageUrl = contentInstance.images
+              ? contentInstance.images[0].url
+              : contentInstance.album.images[0].url;
+      
+            return (
+              <div key={index} className="grid-item">
+                <img src={imageUrl} alt="Grid content" />
+              </div>
+            );
+          })}
+          {includeProfilePicture && profilePictureUrl && (
+            <div className="profile-picture-overlay" style={profilePictureContainerStyle}>
+              <img src={profilePictureUrl} alt="Profile" style={profilePictureStyle} />
+            </div>
+          )}
         </div>
-      )}
-    </div>
-  );
+      );
 };
 
 export default GridDisplay;
