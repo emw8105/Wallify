8512950c3f26f928a0165ddf3934df4f15772bd1
emw8105 <emw8105@gmail.com>
2024-08-22 12:35:42 -0500

    temporary fix for images not being found

--- src/GridDisplay.js
+++ src/GridDisplay.js
@@ -5,34 +5,38 @@ const GridDisplay = ({ content, gridSize, includeProfilePicture, profilePictureU
   // calculate the maximum number of images based on the grid size
   const maxArtists = gridSize.x * gridSize.y;
 
-  // slice the artists array to fit the grid size
-  // NEED TO: handle the case where there are fewer artists than the grid size
+  // slice the content array to fit the grid size
   const contentToDisplay = content.slice(0, maxArtists);
 
   const backgroundStyle = useGradient
-    ? { background: `linear-gradient(to bottom right, ${color1}, ${color2})`,
+    ? {
+        background: `linear-gradient(to bottom right, ${color1}, ${color2})`,
         gridTemplateColumns: `repeat(${gridSize.x}, 1fr)`,
-        gridTemplateRows: `repeat(${gridSize.y}, auto)`, }
-    : {gridTemplateColumns: `repeat(${gridSize.x}, 1fr)`,
-        gridTemplateRows: `repeat(${gridSize.y}, auto)`,};
-        
+        gridTemplateRows: `repeat(${gridSize.y}, auto)`,
+      }
+    : {
+        gridTemplateColumns: `repeat(${gridSize.x}, 1fr)`,
+        gridTemplateRows: `repeat(${gridSize.y}, auto)`,
+      };
+
   const profilePictureContainerStyle = useGradient
-  ? {
-      backgroundImage: `linear-gradient(to bottom right, ${color1}, ${color2})`,
-    }
-  : {};
-  
+    ? {
+        backgroundImage: `linear-gradient(to bottom right, ${color1}, ${color2})`,
+      }
+    : {};
+
   const profilePictureStyle = {
     borderRadius: '50%',
   };
-      
+
+  const defaultImageUrl = 'default-not-found-image.png';
+
   return (
     <div className="grid-container" style={backgroundStyle}>
       {contentToDisplay.map((contentInstance, index) => {
-        const imageUrl = contentInstance.images
-          ? contentInstance.images[0].url
-          : contentInstance.album.images[0].url;
-  
+        // check if the contentInstance has images and fall back to the default image if necessary
+        const imageUrl = contentInstance?.images?.[0]?.url || contentInstance?.album?.images?.[0]?.url || defaultImageUrl;
+
         return (
           <div key={index} className="grid-item">
             <img src={imageUrl} alt="Grid content" />
