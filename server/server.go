82116a15dce92191fafd776d9d240033d44d80b3
Evan Wright <emw8105@gmail.com>
2024-10-14 23:31:47 -0500

    Go server implemented successfully

--- server/server.go
+++ server/server.go
@@ -12,14 +12,17 @@ package main
 // defer resp.Body.Close() - ensures that the response body is closed after the function returns, helps avoid resource leakage
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/hex"
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
+	"strconv"
 	"strings"
 	"sync"
 	"time"
@@ -77,6 +80,241 @@ func cleanupTokenMap() {
 	}
 }
 
+// refresh token route to get a new access token
+func refreshSpotifyToken(refreshToken string) (string, error) {
+	log.Println("Attempting to refresh access token for refresh token:", refreshToken)
+
+	// prep the form data
+	data := url.Values{}
+	data.Set("grant_type", "refresh_token")
+	data.Set("refresh_token", refreshToken)
+
+	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", bytes.NewBufferString(data.Encode()))
+	if err != nil {
+		return "", fmt.Errorf("error creating request: %w", err)
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.SetBasicAuth(clientId, clientSecret)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return "", fmt.Errorf("error sending token request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// parse the response
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return "", fmt.Errorf("error reading response body: %w", err)
+	}
+
+	var responseData map[string]interface{}
+	err = json.Unmarshal(body, &responseData)
+	if err != nil {
+		return "", fmt.Errorf("error parsing response JSON: %w", err)
+	}
+
+	// if the response contains the access token, then return it back to the calling function
+	if accessToken, exists := responseData["access_token"].(string); exists {
+		log.Println("Access token refreshed successfully")
+		return accessToken, nil
+	}
+
+	return "", fmt.Errorf("error refreshing access token: response missing access token")
+}
+
+func enableCors(w *http.ResponseWriter) {
+	(*w).Header().Set("Access-Control-Allow-Origin", "*")
+	(*w).Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	(*w).Header().Set("Access-Control-Allow-Headers", "Content-Type, x-token-key")
+	(*w).Header().Set("Access-Control-Max-Age", "86400")
+}
+
+// universal request handler for Spotify API
+func makeSpotifyRequest(req *http.Request, accessToken, endpoint string, retryCount int) ([]byte, error) {
+	client := &http.Client{}
+	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", accessToken))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("error sending request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, fmt.Errorf("error reading response body: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		if resp.StatusCode == http.StatusUnauthorized && retryCount < 1 {
+			log.Println("Access token expired, attempting to refresh token...")
+
+			tokenKey := req.Header.Get("x-token-key")
+			tokenMapMutex.Lock()
+			tokenInfo, exists := tokenMap[tokenKey]
+			tokenMapMutex.Unlock()
+
+			if !exists {
+				log.Printf("Invalid or missing token")
+				return nil, fmt.Errorf("invalid or missing token")
+			}
+
+			newAccessToken, err := refreshSpotifyToken(tokenInfo["refreshToken"].(string))
+			if err != nil {
+				log.Println("Failed to refresh token, returning error.")
+				return nil, fmt.Errorf("error refreshing access token: %w", err)
+			}
+
+			tokenMapMutex.Lock()
+			tokenMap[tokenKey]["accessToken"] = newAccessToken
+			tokenMapMutex.Unlock()
+
+			req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", newAccessToken))
+			return makeSpotifyRequest(req, newAccessToken, endpoint, retryCount+1)
+		}
+		return nil, fmt.Errorf("spotify API error: %s", string(body))
+	}
+
+	return body, nil
+}
+
+// helper function to get the maximum concatenated 99 items from the user's top artists or tracks
+// Spotify API limit is 50 items per request, each client requests 99, this intermediary function is used to handle the requests
+func getTopContent(accessToken, content string, totalContent int) ([]map[string]interface{}, error) {
+	limit := 50 // Spotify's API limit per request
+	var results []map[string]interface{}
+
+	for offset := 0; offset < totalContent; offset += limit {
+		requestLimit := min(limit, totalContent-offset)
+		url := fmt.Sprintf("https://api.spotify.com/v1/me/top/%s?limit=%d&offset=%d", content, requestLimit, offset)
+
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			return nil, fmt.Errorf("error creating request: %w", err)
+		}
+
+		resp, err := makeSpotifyRequest(req, accessToken, content, 0)
+		if err != nil {
+			return nil, err
+		}
+
+		var data map[string]interface{}
+		if err := json.Unmarshal(resp, &data); err != nil {
+			return nil, fmt.Errorf("error unmarshaling response: %w", err)
+		}
+
+		items, ok := data["items"].([]interface{})
+		if !ok {
+			return nil, fmt.Errorf("unexpected response format")
+		}
+
+		for _, item := range items {
+			results = append(results, item.(map[string]interface{}))
+		}
+	}
+
+	return results[:min(len(results), totalContent)], nil
+}
+
+// route to fetch the user's top artists or tracks based on the specified content type
+func handleTopContent(contentType string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		enableCors(&w)
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		tokenKey := r.Header.Get("x-token-key")
+		log.Printf("Request received for top %s with Token Key %v\n", contentType, tokenKey)
+
+		tokenMapMutex.Lock()
+		tokenInfo, exists := tokenMap[tokenKey]
+		tokenMapMutex.Unlock()
+
+		if !exists {
+			http.Error(w, "Invalid or missing token", http.StatusUnauthorized)
+			log.Printf("Invalid or missing token")
+			return
+		}
+
+		limit := r.URL.Query().Get("limit")
+		if limit == "" {
+			limit = "50"
+		}
+
+		totalContent, err := strconv.Atoi(limit)
+		if err != nil {
+			http.Error(w, "Invalid limit", http.StatusBadRequest)
+			log.Printf("Invalid limit: %v", err)
+			return
+		}
+
+		topContent, err := getTopContent(tokenInfo["accessToken"].(string), contentType, totalContent)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error fetching top %s", contentType), http.StatusInternalServerError)
+			log.Printf("Error fetching top %s: %v", contentType, err)
+			return
+		}
+
+		response, err := json.Marshal(topContent)
+		if err != nil {
+			http.Error(w, "Error marshaling response", http.StatusInternalServerError)
+			log.Printf("Error marshaling response: %v", err)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(response)
+	}
+}
+
+// route to fetch the user's profile picture
+func handleProfile(w http.ResponseWriter, r *http.Request) {
+	enableCors(&w)
+	if r.Method == "OPTIONS" {
+		w.WriteHeader(http.StatusOK)
+		return
+	}
+
+	tokenKey := r.Header.Get("x-token-key")
+	log.Printf("Request received for %v with Token Key %v\n", r.URL.Path, tokenKey)
+
+	tokenMapMutex.Lock()
+	tokenInfo, exists := tokenMap[tokenKey]
+	tokenMapMutex.Unlock()
+
+	if !exists {
+		http.Error(w, "Invalid or missing token", http.StatusUnauthorized)
+		log.Printf("Invalid or missing token")
+		return
+	}
+
+	req, _ := http.NewRequest("GET", "https://api.spotify.com/v1/me", nil)
+	response, err := makeSpotifyRequest(req, tokenInfo["accessToken"].(string), "profile", 0)
+	if err != nil {
+		http.Error(w, "Error fetching profile", http.StatusInternalServerError)
+		return
+	}
+
+	// parse the profile picture, some users may not have a profile picture so lots of checks are needed
+	var profileData map[string]interface{}
+	json.Unmarshal(response, &profileData)
+	profilePictureUrl := ""
+	if images, ok := profileData["images"].([]interface{}); ok && len(images) > 0 {
+		if image, ok := images[0].(map[string]interface{}); ok {
+			if url, ok := image["url"].(string); ok {
+				profilePictureUrl = url
+			}
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(map[string]string{"profilePictureUrl": profilePictureUrl})
+}
+
 // route to handle the callback from Spotify after the user is authenticated
 func handleCallback(w http.ResponseWriter, r *http.Request) {
 	code := r.URL.Query().Get("code")
@@ -94,6 +332,7 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 	data.Set("code", code)
 	data.Set("redirect_uri", redirectUri)
 
+	// make a post request to spotify's access token endpoint
 	req, err := http.NewRequest("POST", "https://accounts.spotify.com/api/token", strings.NewReader(data.Encode()))
 	if err != nil { // i.e. if there is an error, log it and exit
 		log.Fatalf("Error creating request: %v", err)
@@ -116,15 +355,29 @@ func handleCallback(w http.ResponseWriter, r *http.Request) {
 
 	log.Println("Response from Spotify:", string(body))
 
-	// generate a unique key for the token and store it in the token map
+	// parse the JSON response to extract the access token
+	var tokenResponse map[string]interface{}
+	if err := json.Unmarshal(body, &tokenResponse); err != nil {
+		log.Fatalf("Error unmarshalling token response: %v", err)
+	}
+
+	accessToken, ok := tokenResponse["access_token"].(string)
+	if !ok {
+		log.Fatalf("Access token missing from response: %v", tokenResponse)
+	}
+
+	// generate a unique key for the token
 	key := generateUniqueKey()
 
+	// store the token and timestamp in the token map with mutex lock for sync safety
 	tokenMapMutex.Lock()
 	tokenMap[key] = map[string]interface{}{
-		"accessToken": body, // store the actual access token in the token map
+		"accessToken": accessToken,
 		"timestamp":   time.Now(),
 	}
 	tokenMapMutex.Unlock()
+
+	fmt.Println("Token Map: ", tokenMap)
 
 	// redirect the user back to the React app with the token key
 	http.Redirect(w, r, fmt.Sprintf("http://localhost:3000?token_key=%s", key), http.StatusSeeOther)
@@ -165,6 +418,10 @@ func main() {
 
 	// callback route, it's a bit more complicated so details are abstracted to the handleCallback function
 	http.HandleFunc("/callback", handleCallback)
+
+	http.HandleFunc("/top-artists", handleTopContent("artists"))
+	http.HandleFunc("/top-tracks", handleTopContent("tracks"))
+	http.HandleFunc("/profile", handleProfile)
 
 	log.Println("Server is running on http://localhost:8888")
 	log.Fatal(http.ListenAndServe(":8888", nil))
